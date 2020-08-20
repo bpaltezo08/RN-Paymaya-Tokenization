f250601b394d217d5452a96a74e0036fd613524f
bpaltezo08 <bryan.dagusoft@gmail.com>
2020-08-20 11:30:32 +0800

    Create sampleclient.js

---
+++ sampleclient.js
@@ -0,0 +1,34 @@
+export const SampleClient = {
+    "firstName": "John",
+    "middleName": "Paul",
+    "lastName": "Doe",
+    "birthday": "1995-10-24",
+    "customerSince": "1995-10-24",
+    "sex": "M",
+    "contact": {
+      "phone": "+639181008888",
+      "email": "merchant@merchantsite.com"
+    },
+    "shippingAddress": {
+      "firstName": "John",
+      "middleName": "Paul",
+      "lastName": "Doe",
+      "phone": "+639181008888",
+      "email": "merchant@merchantsite.com",
+      "line1": "6F Launchpad",
+      "line2": "Reliance Street",
+      "city": "Mandaluyong City",
+      "state": "Metro Manila",
+      "zipCode": "1552",
+      "countryCode": "PH",
+      "shippingType": "ST"
+    },
+    "billingAddress": {
+      "line1": "6F Launchpad",
+      "line2": "Reliance Street",
+      "city": "Mandaluyong City",
+      "state": "Metro Manila",
+      "zipCode": "1552",
+      "countryCode": "PH"
+    }
+}
